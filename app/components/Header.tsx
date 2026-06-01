c2b6d7bb7acb0a7ed67fb38aefac015f310df736
Juranovitz <juranovitz.oliver@students.jedlik.eu>
2026-06-01 11:56:35 +0200

    utolsó perces fix

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,12 +9,15 @@ export default function Header() {
   if (pathname === "/" || pathname === "") return null;
 
   return (
+    <div className="bg-blue-100 gap-17">
+      <br />
       <button
         onClick={() => router.push("/")}
         className="fixed top-4 right-4 z-50 bg-blue-100 p-3 rounded-lg w-34 h-15 bg-indigo-200 shadow-md shadow-gray-500 text-xl rounded-xl border-3 border-indigo-300 hover:cursor-pointer text-black hover:bg-indigo-400 hover:text-white hover:shadow-lg hover:shadow-gray-400 transition-colors duration-300"
       >
         ➤ Főoldal
       </button>
-
+      <br />
+</div>
   );
 }
